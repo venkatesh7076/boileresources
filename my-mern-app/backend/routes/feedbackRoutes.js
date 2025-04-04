cb3040a14766516f280a6aaadbbebb2da4ce6085
prachag29 <chagantp@purdue.edu>
2025-04-03 22:44:07 -0400

    fixed feedback form + added header

--- my-mern-app/backend/routes/feedbackRoutes.js
+++ my-mern-app/backend/routes/feedbackRoutes.js
@@ -1,23 +1,12 @@
 import express from "express";
-import Feedback from "../models/Feedback.js";
+import "../config/passport.js";
 import nodemailer from "nodemailer";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import Feedback from "../models/Feedback.js";
+
 dotenv.config();
 
 const router = express.Router();
-
-//DEBUGGING
-console.log("ADMIN_EMAIL:", process.env.ADMIN_EMAIL);
-console.log("ADMIN_EMAIL_PASSWORD:", process.env.ADMIN_EMAIL_PASSWORD ? "Present ✅" : "Missing ❌");
-
-// Configure nodemailer for sending emails
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.ADMIN_EMAIL, // Administrator's email address
-    pass: process.env.ADMIN_EMAIL_PASSWORD, // Administrator's email password
-  },
-});
 
 // POST /api/feedback - Submit feedback
 router.post("/", async (req, res) => {
@@ -33,15 +22,34 @@ router.post("/", async (req, res) => {
     const feedback = new Feedback({ name, email, category, message });
     await feedback.save();
 
-    // Send email to the administrator
-    await transporter.sendMail({
-      from: email,
-      to: process.env.ADMIN_EMAIL, // Administrator's email
-      subject: `New Feedback: ${category}`,
+    // Configure nodemailer for sending emails
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER, // Administrator's email address
+        pass: process.env.EMAIL_PASS, // Administrator's email password
+      },
+    });   
+
+    const mailOptions = {
+      from: email, // User's email address
+      to: process.env.EMAIL_USER, // Administrator's email address
+      subject: `Boiler Resources New Feedback: ${category}`,
       text: `Name: ${name}\nEmail: ${email}\nCategory: ${category}\nMessage: ${message}`,
+    };
+
+    // Send email to the administrator
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.error("Error sending email:", error);
+        return res.status(500).json({ error: "Failed to send email." });
+      } else {
+        console.log("Email sent: " + info.response);
+        return res.status(200).json({ message: "Feedback submitted successfully." });
+      }
     });
-    console.log("Email sent successfully!");
-    res.status(201).json({ message: "Feedback submitted successfully." });
+    //console.log("Email sent successfully!");
+    //res.status(201).json({ message: "Feedback submitted successfully." });
   } catch (err) {
     console.error("Error saving feedback or sending email:", err);
     res.status(500).json({ error: "Failed to submit feedback." });
